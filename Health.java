b4066c61a281e3010cdb83061703a3308613e6e2
Danielbril2 <104306248+Danielbril2@users.noreply.github.com>
2022-06-14 01:05:08 +0300

    Add files via upload

--- Health.java
+++ Health.java
@@ -1,11 +1,22 @@
 package com.company;
 
 public class Health {
-    private int healthPool;
-    private int healthAmount;
+    private int healthPool; //max health
+    private int healthAmount; //curr health
 
     public Health(int healthPool, int healthAmount){
         this.healthPool = healthPool;
+        this.healthAmount = healthAmount;
+    }
+
+    public int getHealthAmount() {return healthAmount;}
+    public int getHealthPool() {return healthPool;}
+
+    public void setHealthPool(int healthPool){
+        this.healthPool = healthPool;
+    }
+
+    public void setHealthAmount(int healthAmount){
         this.healthAmount = healthAmount;
     }
 }
